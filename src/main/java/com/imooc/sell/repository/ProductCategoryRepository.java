66b921b6b8d0adee3519d08608117260baf7f4c8
TyronnLueXinyu <tyronnluexinyu@163.com>
2018-12-05 10:16:04 +0800

    1 commit

---
+++ src/main/java/com/imooc/sell/repository/ProductCategoryRepository.java
@@ -0,0 +1,14 @@
+package com.imooc.sell.repository;
+
+import com.imooc.sell.dataobject.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @Author: TyronnLue
+ * @Date: 2018-11-20 16:10
+ */
+public interface ProductCategoryRepository  extends JpaRepository<ProductCategory,Integer>{
+
+
+
+}
---
+++ src/test/java/com/imooc/sell/repository/ProductCategoryRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.imooc.sell.repository;
+
+import com.imooc.sell.dataobject.ProductCategory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author: TyronnLue
+ * @Date: 2018-12-4 9:31
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductCategoryRepositoryTest {
+    @Autowired
+    private ProductCategoryRepository repository;
+
+    @Test
+    public void findOneTest(){
+        ProductCategory productCategory = repository.findOne(1);
+        System.out.println(productCategory.toString());
+        System.out.println(134113);
+    }
+
+}
